20a6cca3e60f095569959068ceda4c4390cf8e97
root <root@c1r2s2.42antananarivo.mg>
2026-01-14 21:16:19 +0000

    test

--- srcs/services/code/tools/entrypoint.sh
+++ srcs/services/code/tools/entrypoint.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-sudo chown -R ballain:ballain /workspace
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+cp /root/.zshrc $HOME/.zshrc
+chsh -s $(which zsh) root
 code --wait --no-sandbox --password-store=basic --user-data-dir=/workspace/.vscode/user-data --extensions-dir=/workspace/.vscode/extensions .
